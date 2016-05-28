f604908459fc94ddc56623799f665a677f3499c7
elgrim312 <alexandre.vagnair@hetic.net>
2016-05-28 14:29:02 +0200

    ?

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -27,6 +27,7 @@ $(function () {
 
         var recognition = new webkitSpeechRecognition();
         recognition.lang = "fr-FR";
+        var text = '';
         recognition.continuous = false;
         recognition.interimResults = true;
         // Début enregistrement vocal
