459fa46cd2c3537fdaddf9ed3a4f2aeb6eceb2b6
elgrim312 <alexandre.vagnair@hetic.net>
2016-06-03 10:45:29 +0200

    ad route

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('landing', { title: 'Projet JS' });
 });
+router.get('/interface', function (req, res, next ) {
+    res.render('interface', {title: 'interface'})
+})
 
 module.exports = router;
