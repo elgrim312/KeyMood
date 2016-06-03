ff0f6025873b687c7245f2f1d68421cb28e787f9
elgrim312 <alexandre.vagnair@hetic.net>
2016-06-03 14:23:25 +0200

    clear  bug  yes

--- public/javascripts/player.js
+++ public/javascripts/player.js
@@ -2,6 +2,7 @@
  * Created by alexandrevagnair on 03/06/2016.
  */
 var title =  $('title').html();
+console.log(title);
 $.urlParam = function(name){
     var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
     return results[1] || 0;
@@ -11,6 +12,7 @@ var mot1 =$.urlParam('mot1'); // name
 var mot2 =$.urlParam('mot2'); // name
 var mot3 =$.urlParam('mot3'); // name
 var tab = [mot1, mot2, mot3];
+console.log(tab)
 var random_Tab = Math.floor(Math.random() * tab.length);
 // prend un mot dans l'index
 var result_Finale = tab[random_Tab];
--- routes/index.js
+++ routes/index.js
@@ -3,20 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-<<<<<<< HEAD
   res.render('landing', { title: 'Keymood | Accueil' });
 });
 router.get('/interface', function (req, res, next ) {
     res.render('interface', {title: 'Keymood | Interface'})
 });
-router.get('/interface', function (req, res, next ) {
+router.get('/player', function (req, res, next ) {
     res.render('player', {title: 'Keymood | Player'})
 });
 router.get('/interface', function (req, res, next ) {
     res.render('stat', {title: 'Keymood | Statistiques'})
-=======
-  res.render('landing', { title: 'Projet JS' });
->>>>>>> alexandre_hello
 });
 
 module.exports = router;
